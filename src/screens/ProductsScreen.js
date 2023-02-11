6a1bdd5c0f7933124da2381eda3659164d77c564
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2023-02-11 15:45:09 +0400

    implement homePage and productsScreen

---
+++ src/screens/ProductsScreen.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { FlatList, Image, StyleSheet, View } from 'react-native';
+import products from '../data/products';
+
+const ProductsScreen = () => {
+  return (
+    <FlatList
+      data={products}
+      renderItem={({ item }) => (
+        <View style={styles.itemContainer}>
+          <Image source={{ uri: item.image }} style={styles.image} />
+        </View>
+      )}
+      numColumns={2}
+    />
+  )
+}
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    width: '50%',
+    padding: 1,
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+  }
+});
+
+export default ProductsScreen;
