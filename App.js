556b045f080a2cb83b006c818238ebd78e0c593c
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2023-02-26 01:08:59 +0400

    remove useless code

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
 import { Provider } from 'react-redux';
 import Navigation from './src/navigation';
 import { store } from './src/store';
@@ -7,17 +6,8 @@ import { store } from './src/store';
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Navigation />
-        <StatusBar style="auto" />
-      </View>
+      <Navigation />
+      <StatusBar style="auto" />
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
